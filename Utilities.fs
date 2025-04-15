8cb57be6d590d1924af8966f61e93a1461186b5a
ZatolochnyiD <d.zatolochnyi@gmail.com>
2025-04-15 09:40:18 +0300

    Renamed utilities.

--- Utilities.fs
+++ Utilities.fs
@@ -1,13 +1,9 @@
 module Minotaur.Utilities.Misc
 open System
 open System.Linq
-open Minotaur.Utilities.Vector
 
 let floorToInt (x: double) =
     x |> Math.Floor |> int
-
-let vectorTupleEqual (vector: Vector) (x: int, y: int) =
-    vector.x = x && vector.y = y
 
 let center sideSize =
     let shift = -1 + sideSize % 2
